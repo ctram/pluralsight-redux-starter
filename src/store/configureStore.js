4d4da50f0df12aeb99211116f4abf88cefea490a
Chris Tram <chris.tram@gmail.com>
2018-01-03 15:59:05 -0800

    refactor reducer for all courses

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import reduxImmtableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, applyMiddleware());
+  return createStore(rootReducer, initialState, applyMiddleware(thunk, reduxImmtableStateInvariant()));
 }
